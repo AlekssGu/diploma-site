58622a5d208f4b822ada8e52bca02bf9c116579f
Aleksandrs Gusevs <gusevs.aleksandrs@gmail.com>
2014-03-06 08:38:12 +0200

    Pirmas lapas sakotnejais izskats

--- README.md
+++ README.md
@@ -1,38 +1,23 @@
-#FuelPHP
+#Informācijas Sistēma Pilsētas ūdens
 
-* Version: 1.7
-* [Website](http://fuelphp.com/)
-* [Release Documentation](http://docs.fuelphp.com)
-* [Release API browser](http://api.fuelphp.com)
-* [Development branch Documentation](http://dev-docs.fuelphp.com)
-* [Development branch API browser](http://dev-api.fuelphp.com)
-* [Support Forum](http://fuelphp.com/forums) for comments, discussion and community support
+* Versija: 0
+* Statuss: darbs vēl nav uzsākts
+* [WWW](https://udens.agusevs.com)
 
-## Description
+## Apraksts
 
-FuelPHP is a fast, lightweight PHP 5.3 framework. In an age where frameworks are a dime a dozen, We believe that FuelPHP will stand out in the crowd.  It will do this by combining all the things you love about the great frameworks out there, while getting rid of the bad.
+IS Pilsētas ūdens ir paredzēta uzņēmumiem, kuru pakalpojumu klāstā ietilpst 
+kanalizācijas un ūdensapgādes tīklu projektēšana, būvēšana un uzturēšana. 
+Šī sistēma ir orientēta uz klientu un viņa ērtību. Klientam ir iespēja ievadīt ūdens skaitītāja mērījumus, sazināties ar uzņēmuma pārstāvjiem, kā arī uzdot dažādus viņam interesējošus jautājumus, uz kuriem atbildēs uzņēmuma darbinieki.
 
-## More information
+##Izstrādātāji
 
-For more detailed information, see the [development wiki](https://github.com/fuelphp/fuelphp/wiki).
+* Aleksandrs Gusevs, Latvijas Universitātes Datorikas fakultātes 2. kursa students.
 
-##Development Team
+###Izstrādes process
 
-* Harro Verton - Project Manager, Developer ([http://wanwizard.eu/](http://wanwizard.eu/))
-* Frank de Jonge - Developer ([http://frenky.net/](http://frenky.net/))
+Darbs tiek izstrādāts iteratīvi (pa posmiem). Katrs posms tiek identificēts ar versijas numuru un izmaiņām, kas konkrētajā versijā tikušas izstrādātas.
 
-### Want to join?
+###Izmaiņas:
 
-The FuelPHP development team is always looking for new team members, who are willing
-to help lift the framework to the next level, and have the commitment to not only
-produce awesome code, but also great documentation, and support to our users.
-
-You can not apply for membership. Start by sending in pull-requests, work on outstanding
-feature requests or bugs, and become active in the #fuelphp IRC channel. If your skills
-are up to scratch, we will notice you, and will ask you to become a team member.
-
-### Alumni
-
-* Jelmer Schreuder - Developer ([http://jelmerschreuder.nl/](http://jelmerschreuder.nl/))
-* Phil Sturgeon - Developer ([http://philsturgeon.co.uk](http://philsturgeon.co.uk))
-* Dan Horrigan - Founder, Developer ([http://dhorrigan.com](http://dhorrigan.com))
+* 0. versija: izveidots serveris ar HTTP/HTTPS/SSH piekļuvi, kur tika uzinstalēts webserveris un FuelPHP ietvars.
---
+++ fuel/app/views/template.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Informācijas sistēma \"Pilsētas ūdens\"">
+        <meta name="author" content="Aleksandrs Gusevs">
+	<title><?php echo $title; ?></title>
+	<?php echo Asset::css('bootstrap.min.css'); ?>
+        <?php echo Asset::css('style.css'); ?>
+        <?php echo Asset::js('jquery.min.js'); ?>
+        <?php echo Asset::js('bootstrap.js'); ?>
+</head>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-47792219-1', 'agusevs.com');
+  ga('send', 'pageview');
+
+</script>
+
+<!-- Aktivizē slīdrādi -->
+<script>
+$('.slider').carousel({
+    interval: 5000 //changes the speed
+})
+</script>
+
+<body>
+    <!-- navigācijas panelis -->
+    <div class="navbar navbar-default">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="visible-xs navbar-brand" href="/">Pilsētas ūdens</a>
+        </div>
+        <div class="text-center navbar-collapse collapse navbar-responsive-collapse">
+
+            <div id="nav-links" class="row">
+                <div class="col-lg-3">
+                    <a href="/" type="button" class="btn btn-link">Sākums</a>
+                </div>
+                <div class="col-lg-3">
+                    <a href="/" type="button" class="btn btn-link disabled">Lapa 2</a>
+                </div>
+                <div class="col-lg-3">
+                    <a href="/" type="button" class="btn btn-link disabled">Lapa 3</a>
+                </div>
+                <div class="col-lg-3">
+                    <a href="/" type="button" class="btn btn-link disabled">Lapa 4</a>
+                </div>
+            </div>
+
+        </div>
+      </div>
+    <!-- /navigācijas panelis -->
+
+    
+    <!-- lapas ietvars -->
+        <?php echo $content; ?>
+    <!-- /lapas ietvars -->
+    
+    <!-- lapas kājene -->
+    <div class="container">
+        <hr> 
+        <div class="text-right col-lg-12">
+            <ul class="list-inline">
+                <li><a href='#' class="text-muted">Ziņot par kļūdu</a></li>
+                <li>Aleksandrs Gusevs &copy; <?php echo Date::time()->format('%Y'); ?></li>
+            </ul>
+        </div>
+    </div
+    <!-- /lapas kājene -->
+    
+</body>
+</html>
