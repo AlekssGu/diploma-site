0666bdc3592cec5a19f83161d3309c10da5b0307
Aleksandrs Gusevs <gusevs.aleksandrs@gmail.com>
2014-03-09 22:16:25 +0200

    e-pasta konfiguracija

--- fuel/app/views/emails/regconfirm.php
+++ fuel/app/views/emails/regconfirm.php
@@ -9,13 +9,12 @@
 </head>
  
 <body bgcolor="#FFFFFF">
-
-<h6>Šis ziņa jums tika nosūtīta, jo jūs reģistrējāties sistēmā "Pilsētas ūdens". Ja jūs to nedarījāt, ignorējiet šo e-pastu</h6>
 <h2>Paldies par reģistrāciju!</h2>
+<h6>Šī ziņa jums tika nosūtīta, jo jūs reģistrējāties sistēmā "Pilsētas ūdens". Ja jūs to nedarījāt, ignorējiet šo e-pastu.</h6>
 <br><br>
-<h3><a style="margin-bottom:15px" target="_blank" href="https://udens.agusevs.com/confirm/&lt;?php echo $code;?&gt;" title="Apstiprināt reģistrāciju">Apstiprināt reģistrāciju</a></h3>
+<h3><a style="margin-bottom:15px" target="_blank" href="https://udens.agusevs.com/confirm/&lt;?php echo $code;?&gt;" title="Apstiprināt reģistrāciju">&laquo; Apstiprināt reģistrāciju &raquo;</a></h3>
 <br><br>
 <h6>Ja saite nestrādā, tad ievadiet adresi https://udens.agusevs.com/confirm/<?php echo $code;?> jūsu interneta pārlūka adreses vietā</h6>
-<p style="margin-top:10px">Ar cieņu, <br>IS "Pilsētas ūdens" administrācija.</p>
+<p style="margin:10px">Ar cieņu, <br>IS "Pilsētas ūdens" administrācija.</p>
 </body>
 </html>
