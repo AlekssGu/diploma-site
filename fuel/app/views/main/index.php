cc1f1de8f79b9cf39a1fa27e1316846d4176fbaf
Aleksandrs Gusevs <gusevs.aleksandrs@gmail.com>
2014-03-14 00:38:31 +0200

    lietotaja panelis

--- fuel/app/views/main/index.php
+++ fuel/app/views/main/index.php
@@ -1,4 +1,54 @@
         <?php if(Auth::check()) { // Lietotāju grupas "Klients" sākumlapas bloks ?>  
+<script src="http://code.highcharts.com/highcharts.js"></script>
+<script>
+$(function () {
+        $('#container').highcharts({
+            title: {
+                text: 'Ūdens patēriņš',
+                x: -20 //center
+            },
+            //subtitle: {
+            //    text: 'Par gadu',
+            //    x: -20
+            //},
+            xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun',
+                    'Jūl', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec']
+            },
+            yAxis: {
+                title: {
+                    text: 'Patēriņš (m3)'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            credits: {
+                enabled: false
+            },
+            tooltip: {
+                
+                valueSuffix: 'm3'
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: [{
+                name: 'Ūdensapgāde',
+                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            }, {
+                name: 'Notekūdeņi',
+                data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+            }]
+        });
+    });
+    
+</script>
             <div style="margin-top:20px" class="container">
                 <div class="row">
                     <div class="col-md-3">
@@ -14,7 +64,30 @@
                         <a href="/user/logout" class="btn btn-block btn-default" title="Iziet no sistēmas"><span class="glyphicon glyphicon-log-out"></span> Iziet</a>
                     </div>
                 </div>
-            </div>
+                <div class="row" style="margin-top:20px">
+                    <div class="col-md-6">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <h3 class="text-center">Mana pēdējā aktivitāte</h3>
+                                <hr/>
+                                <h4>Iesniegtais mērījums:</h4>
+                                <p>Pēdējais mērījums: 000413<span class="text-danger">45</span></p>
+                                <h4>Veiktie maksājumi:</h4>
+                                <p>Pēdējais maksājums: 13.03.2014</p>
+                                <h4>Saņemtie pakalpojumi:</h4>
+                                <p>Pēdējais pakalpojums: Nosēdbedres likvidācija</p>
+                            </div>
+                        </div>
+                        <div class='text-center'>
+                        <a href="#" class="btn btn-info">Ievadīt mērījumu</a>
+                        <a href="#" class="btn btn-info">Paziņot par bojājumu</a>
+                        <a href="#" class="btn btn-info">Pasūtīt pakalpojumus</a>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+                    </div>
+                </div>
         <?php } else { // Lietotāju grupas "Viesis" sākumlapas bloks ?>
 
             <!-- Galvenās lapas saturs -->
